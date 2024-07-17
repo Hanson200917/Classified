dc60ef6a48000dfba5aa669a4f6eceb66dc28b15
Hanson200917 <hansonsy2018@gmail.com>
2024-07-17 16:56:13 +0100

    Add files via upload

---
+++ classified.py
@@ -0,0 +1,115 @@
+while True:
+    password = input('User:')
+    if password == ('shingheisy'):
+        print ('ACCESS GRANTED')
+        break
+    else:
+        print('ACCESS DENIED')
+
+
+while True:
+    password = input('Level 1 Clearance:')
+    if password == ('hanson'):
+        print ('ACCESS GRANTED')
+        break
+
+    else:
+        print('ACCESS DENIED')
+
+
+while True:
+    password = input('Level 1 Files:')
+    if password == ('Level 2 files'):
+        print ('ACCESS GRANTED')
+        break
+    elif password == ('lvl 2 files'):
+        print ('ACCESS GRANTED')
+        break
+    elif password == ('level 2 files'):
+        print ('ACCESS GRANTED')
+        break
+    else:
+        print('File not found')
+
+
+
+while True:
+    password = input('Level 2 Clearance:')
+    if password == ('200917'):
+        print ('ACCESS GRANTED')
+        break
+    else:
+        print('ACCESS DENIED')
+
+
+while True:
+    password = input('Level 2 Files:')
+    if password == ('Level 3 files'):
+        print ('ACCESS GRANTED')
+        break
+    elif password == ('lvl 3 files'):
+        print ('ACCESS GRANTED')
+        break
+    elif password == ('level 3 files'):
+        print ('ACCESS GRANTED')
+        break
+    else:
+        print('File not found')
+
+
+while True:
+    password = input('Level 3 Clearance:')
+    if password == ('hanson107'):
+        print ('ACCESS GRANTED')
+        break
+    else:
+        print('ACCESS DENIED')
+
+
+while True:
+    password = input('Level 3 Files:')
+    if password == ('Level 4 files'):
+        print ('ACCESS GRANTED')
+        break
+    elif password == ('lvl 4 files'):
+        print ('ACCESS GRANTED')
+        break
+    elif password == ('level 4 files'):
+        print ('ACCESS GRANTED')
+        break
+    else:
+        print('File not found')
+
+
+while True:
+    password = input('Level 4 Clearance:')
+    if password == ('Vs763542'):
+        print ('ACCESS GRANTED')
+        break
+    else:
+        print('ACCESS DENIED')
+
+
+while True:
+    password = input('Level 4 Files:')
+    if password == ('Level 5 files'):
+        print ('ACCESS GRANTED')
+        break
+    elif password == ('lvl 5 files'):
+        print ('ACCESS GRANTED')
+        break
+    elif password == ('level 5 files'):
+        print ('ACCESS GRANTED')
+        break
+    else:
+        print('File not found')
+
+
+while True:
+    password = input('Level 5 Clearance:')
+    if password == ('hanson1072009hanson'):
+        print ('ACCESS GRANTED')
+        break
+    else:
+        print('ACCESS DENIED')
+    
